9fc097cec46ad03185a269fe39d98b8ece0b7be8
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-03-08 20:46:49 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {Analytics} from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
   document.getElementById("root")
 );
