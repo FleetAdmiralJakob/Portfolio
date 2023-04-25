70a8fec8b22ea0c701564ece8aef986c0dc573ef
Jakob Rössner <jakob.roessner@outlook.de>
2023-04-25 19:45:28 +0200

    .

--- src/components/About/Github.tsx
+++ src/components/About/Github.tsx
@@ -12,7 +12,34 @@ function Github() {
         username="FleetAdmiralJakob"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        colorScheme="dark"
+          // Use the 5 colors: export type ColorScale = [
+          // 	level0: Color,
+          // 	level1: Color,
+          // 	level2: Color,
+          // 	level3: Color,
+          // 	level4: Color
+          // ];
+          // export type ThemeInput = {
+          // 	light: ColorScale | [
+          // 		from: Color,
+          // 		to: Color
+          // 	];
+          // 	dark?: ColorScale | [
+          // 		from: Color,
+          // 		to: Color
+          // 	];
+        theme={
+            {
+                light: [
+                    "#ebebeb",
+                    "#b37be4",
+                    "#9667c1",
+                    "#7a549d",
+                    "#5e417a"
+                ],
+                }
+        }
         fontSize={16}
       />
     </Row>
