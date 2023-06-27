3f2faf7b25538d361ca60a9a9e361851b8650725
Jakob Rössner <jakob@lylu.de>
2023-06-27 19:33:55 +0200

    Added a link to the hits.sh website

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -155,14 +155,15 @@ function NavBar() {
             </Nav.Item>
 
             <Nav.Item className="fork-btn">
-              <img
-                src={
-                  "https://hits.sh/www.roessner.tech.svg?style=for-the-badge&label=Views&extraCount=1254&color=555555&labelColor=711ea6"
-                }
-                className="img-fluid hits"
-                alt="brand"
-              />
-              {/* Visit https://github.com/silentsoft/hits for more info. */}
+              <a href="https://hits.sh/www.roessner.tech/">
+                <img
+                  src={
+                    "https://hits.sh/www.roessner.tech.svg?style=for-the-badge&label=Views&color=555555&labelColor=711ea6"
+                  }
+                  className="img-fluid hits"
+                  alt="brand"
+                />
+              </a>
             </Nav.Item>
 
             <Nav.Item className="fork-btn">
