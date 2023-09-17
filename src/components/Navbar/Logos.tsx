98ded5896128a4cb757c94c53738d14a18b4c19b
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-09-17 15:16:12 +0200

    Switched to pnpm and removed the hacker effect

--- src/components/Navbar/Logos.tsx
+++ src/components/Navbar/Logos.tsx
@@ -1,79 +1,58 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import StandardLogo from "./StandardLogo";
-import { AiFillStar } from "react-icons/ai";
-import { githubData } from "../data";
+import {AiFillStar} from "react-icons/ai";
+import {githubData} from "../data";
 
-class Logos extends React.Component {
-  state: { currentLogo: number; stars: string } = {
-    stars: "",
-    currentLogo: 0,
+const Logos = () => {
+  const [currentLogo, setCurrentLogo] = useState(0);
+  const [stars, setStars] = useState("");
+  const interval = useRef();
+
+  useEffect(() => {
+    interval.current = setInterval(() => renderAnotherClass(), 3000);
+
+    githubData("Portfolio").then(data => {
+      const starCount = data.stargazers_count;
+      setStars(starCount);
+    });
+
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  const renderAnotherClass = () => {
+    requestAnimationFrame(() => {
+      setCurrentLogo(prevLogo => (prevLogo + 1) % 3);
+    });
   };
 
-  // Define the interval property
-  interval = null;
+  const logos = [
+    <StandardLogo />,
+    <AiFillStar style={{ color: "yellow" }} className="star" />
+  ];
 
-  componentDidMount() {
-    // Set an interval to call the renderAnotherClass function every 3 seconds
-    // @ts-ignore
-    this.interval = setInterval(() => this.renderAnotherClass(), 3000);
+  const logoIndex = currentLogo % logos.length;
 
-    githubData("Portfolio").then((data) => {
-      const stars = data.stargazers_count;
-      this.setState({ stars: stars });
-    });
-  }
-
-  componentWillUnmount() {
-    // Clear the interval when the component is unmounted
-    // @ts-ignore
-    clearInterval(this.interval);
-  }
-
-  renderAnotherClass() {
-    // Use requestAnimationFrame to schedule the update of the currentClass in the state
-    requestAnimationFrame(() => {
-      this.setState((prevState) => ({
-        // @ts-ignore
-        currentLogo: (prevState.currentLogo + 1) % 3,
-      }));
-    });
-  }
-
-  render() {
-    // Create an array of React logos to render
-    const logos = [
-      <StandardLogo />,
-      <AiFillStar style={{ color: "yellow" }} className="star" />,
-    ];
-
-    // Get the current logo to render from the state
-    const currentLogo = this.state.currentLogo % logos.length;
-
-    if (currentLogo === 1) {
-      if (
-        this.state.stars === "" ||
-        this.state.stars === null ||
-        this.state.stars === undefined ||
-        !this.state.stars
-      ) {
-        return (
+  if (logoIndex === 1) {
+    if (!stars) {
+      return (
           <div>
-            <div>Check out {logos[currentLogo]} on GitHub!</div>
+            <div>Check out {logos[logoIndex]} on GitHub!</div>
           </div>
-        );
-      } else {
-        return (
+      );
+    } else {
+      return (
           <div>
             <div>
-              {logos[currentLogo]} {this.state.stars}
+              {logos[logoIndex]} {stars}
             </div>
           </div>
-        );
-      }
-    } else {
-      return <div>{logos[currentLogo]}</div>;
+      );
     }
+  } else {
+    return <div>{logos[logoIndex]}</div>;
   }
-}
+};
 
 export default Logos;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-// @ts-ignore
 import logo from "../../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import {Link} from "react-router-dom";
@@ -15,45 +14,6 @@ import linkData from "../data";
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
   const [navColour, updateNavbar] = useState(false);
-
-  const hackerObjects = document.querySelectorAll(
-    "[id^='hacker-object']"
-  ) as NodeListOf<HTMLElement>;
-
-  const lettersSmall = "abcdefghijklmnopqrstuvwxyz";
-
-  hackerObjects.forEach((hackerObject) => {
-    let iterations = 0;
-    let animationFrame: number | null = null;
-
-    hackerObject.addEventListener("mouseover", () => {
-      iterations = 0;
-      cancelAnimationFrame(animationFrame!);
-
-      const animate = () => {
-        hackerObject.innerHTML = hackerObject.innerHTML
-          .split("")
-          .map((letter, index) => {
-            if (index < iterations) {
-              // @ts-ignore
-              return hackerObject.dataset.value[index];
-            }
-            return lettersSmall[Math.floor(Math.random() * 26)];
-          })
-          .join("");
-
-        // @ts-ignore
-        if (iterations >= hackerObject.dataset.value.length) return;
-
-        iterations += 1 / 4;
-        setTimeout(() => {
-          animationFrame = requestAnimationFrame(animate);
-        }, 40);
-      };
-
-      animationFrame = requestAnimationFrame(animate);
-    });
-  });
 
   function scrollHandler() {
     if (window.scrollY >= 20) {
@@ -92,7 +52,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
                 <AiOutlineHome style={{ marginBottom: "2px" }} />
-                <div id="hacker-object" data-value="Home">
+                <div>
                   Home
                 </div>
               </Nav.Link>
@@ -105,7 +65,7 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineUser style={{ marginBottom: "2px" }} />
-                <div id="hacker-object2" data-value="About">
+                <div>
                   About
                 </div>
               </Nav.Link>
@@ -120,7 +80,7 @@ function NavBar() {
                 <AiOutlineFundProjectionScreen
                   style={{ marginBottom: "2px" }}
                 />{" "}
-                <div id="hacker-object3" data-value="Projects">
+                <div>
                   Projects
                 </div>
               </Nav.Link>
@@ -133,7 +93,7 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} />
-                <div id="hacker-object4" data-value="Resume">
+                <div>
                   Resume
                 </div>
               </Nav.Link>
@@ -146,7 +106,7 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <CgMail style={{ marginBottom: "2px" }} />
-                <div id="hacker-object5" data-value="Contact Me">
+                <div>
                   Contact Me
                 </div>
               </Nav.Link>
